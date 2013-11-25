23a2b96a1da0fc3c0f0c2953b0a81ce11439122e
Nemeth Zsolt <furion1@gmail.com>
2013-11-24 20:41:53 -0500

    init

---
+++ hovatovabb.js
@@ -0,0 +1,38 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to hovatovabb.";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+Router.configure({
+    layoutTemplate: 'layout',
+    notFoundTemplate: 'notFound',
+    loadingTemplate: 'loading'
+});
+
+Router.map(function () {
+    this.route('home', {
+        path: '/',
+        template: 'intro'
+    });
+
+    this.route('add', {
+        path: '/add',
+        template: 'add'
+    });
+
+});
